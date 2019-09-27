0293bb291cd1daf36a2acd51b9cbbc846ef05b75
ncfcdaniel <ncfcdaniel@outlook.com>
2019-09-27 15:58:54 +0100

    Creation of rectangle class

---
+++ Rectangle.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace  basic_shapes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           double height = 0.0;
+           double width = 0.0;
+           double area = 0.0;
+            area = height * width;
+        }
+
+        GetArea()
+        {
+           Console.WriteLine("Area of rectangle 1:", area)  ;
+        }
+    }
+}
+
